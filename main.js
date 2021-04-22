ccbd8ada916cbd1844bc836db3f302f890e95d9f
YurleyTorres <marly.torres.silva@gmail.com>
2021-04-21 20:34:19 -0500

    2021-04-09

---
+++ main.js
@@ -0,0 +1,14 @@
+
+let nombre= "Marly Yurley Torres Silva";
+let numero= 33;
+let bool= true;
+const tipo= "Javascript";
+
+console.log(nombre);
+console.log(" tipo de dato: ",typeof(nombre));
+console.log(numero);
+console.log(" tipo de dato: ",typeof(numero));
+console.log(bool);
+console.log(" tipo de dato: ",typeof(bool));
+console.log(tipo);
+console.log(" tipo de dato: ",typeof(tipo));
